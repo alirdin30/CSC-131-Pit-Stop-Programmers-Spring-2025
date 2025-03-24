a299ac5d68768b0aa39081b8b575383f9035248b
zacbemis <zac.bemis916@gmail.com>
2025-03-24 11:31:51 -0700

    I refactored the code for react, the css is consolidated into the App.css file. The navigation bar is now in a seperate file, you can find the images in the assets folder.

---
+++ client/src/pages/Account.jsx
@@ -0,0 +1,16 @@
+import Navigation from '../components/Navigation';
+
+const Account = () => {
+  return (
+    <div className="account-page">
+      <Navigation />
+
+      <section className="account">
+        <h1>Account Page</h1>
+        {/* Add login form and other account-related components here */}
+      </section>
+    </div>
+  );
+};
+
+export default Account;
