5ab9351f5caa444d5097b0a33ad11fd6b7f0b6f6
dillonphan5 <dillonphan@csus.edu>
2025-03-26 22:32:35 -0700

    Task 41 implemented need to test if it actually works

--- client/src/pages/servicepage.js
+++ client/src/pages/servicepage.js
@@ -1,3 +1,8 @@
+/* mongo db initialization? not sure if i need these the slides didnt have these
+var MongoClient = require('mongodb').MongoClient;
+var url = "mongodb://localhost:27017/";
+i put these inside comments so i can get rid of the comments if i need them */
+
 /* boolean variables for service selection */
 /* NOTE: potential bug in the future, 
 leaving or refreshing the page may not reset variables
@@ -77,5 +82,52 @@ function emissionselect() {
 }
 
 function confirmservice(){
-    /* TODO: save each variable so that the next pages can see what was selected */
+    /*  
+    TODO: save each variable so that the next pages can see what was selected 
+    i was thinking when the service is confirmed it will put all of the services
+    into a collection, so for example: if I had oil change and brake service selected
+    a database collection will be created that has "Oil: yes" and "Brake: yes" inside of it.
+    not sure if I want the database to have stuff like "Transmission Fluid: No" or just leave it blank.
+
+    TODO: we don't have a task for having a page for actually viewing the revenue
+    and viewing how much of each service is sold
+
+    NOTE: none of the mongodb code has been tested yet 
+    not sure if this code works or if I did it properly
+
+    the first line of the code will save an entry into (servicename)Total
+    this will be used to keep track of the total amount of times a service is sold
+    to access the information you would just check the size of the collection
+
+    */
+    if (soil === true){
+        db.oilTotal.insert({service: yes});
+    }
+    if (sair === true){
+        db.airTotal.insert({service: yes});
+    }
+    if (stire === true){
+        db.tireTotal.insert({service: yes});
+    }
+    if (sbat === true){
+        db.batTotal.insert({service: yes});
+    }
+    if (sbrake === true){
+        db.brakeTotal.insert({service: yes});
+    }
+    if (ssus === true){
+        db.susTotal.insert({service: yes});
+    }
+    if (sfuel === true){
+        db.fuelTotal.insert({service: yes});
+    }
+    if (salt === true){
+        db.altTotal.insert({service: yes});
+    }
+    if (strans === true){
+        db.transTotal.insert({service: yes});
+    }
+    if (semission === true){
+        db.emissionTotal.insert({service: yes});
+    }
 }
