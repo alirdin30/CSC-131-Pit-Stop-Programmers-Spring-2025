7fb9009bc15cbc51d950d17bee840937112a1a1a
dillonphan5 <dillonphan@csus.edu>
2025-03-29 13:52:32 -0700

    updated, still working on it

--- client/src/pages/Revenue.jsx
+++ client/src/pages/Revenue.jsx
@@ -1,12 +1,20 @@
 import Navigation from "../components/Navigation";
 
+var oilsold = 0;
+var tiresold = 0;
+var brakesold = 0;
+var enginesold = 0;
+var transmissionsold = 0;
+var batterysold = 0;
+
+// prices are currently hardcoded
 const servicenames = [
-  { name: "Oil Change Sales" },
-  { name: "Tire Rotation Sales" },
-  { name: "Brake Service Sales" },
-  { name: "Engine Diagnostics Sales" },
-  { name: "Transmission Services Sales" },
-  { name: "Battery Replacement Sales" },
+  { name: "Oil Change Sales", sold: oilsold, revenue: oilsold * 39.99},
+  { name: "Tire Rotation Sales", sold: tiresold , revenue: tiresold * 49.99},
+  { name: "Brake Service Sales", sold: brakesold, revenue: brakesold * 89.99},
+  { name: "Engine Diagnostics Sales",sold: enginesold , revenue: enginesold * 79.99},
+  { name: "Transmission Services Sales",sold: transmissionsold, revenue: transmissionsold * 129.99},
+  { name: "Battery Replacement Sales", sold: batterysold, revenue: batterysold * 99.99},
 ];
 
 const Revenue = () => {
@@ -19,8 +27,8 @@ const Revenue = () => {
       {servicenames.map((service, index) => (
         <div key={index} className="revenue-item">
           <h3>{service.name}</h3>
-          <p>Amount Sold: </p>
-          <p>Total Revenue: $XXXX</p>
+          <p>Amount Sold: {service.sold}</p>
+          <p>Total Revenue: ${service.revenue}</p>
           <p>Total Cost: $XXXX</p>
           <p>Total Profit: $XXXX</p>
         </div>
