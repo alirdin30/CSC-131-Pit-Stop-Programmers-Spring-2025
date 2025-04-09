c91416d856312d3b9434dd80df6ef8f35a2f88f4
zacbemis <zac.bemis916@gmail.com>
2025-04-08 21:17:41 -0700

    Add employee management features

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -11,6 +11,7 @@ import ForgotPassword from './pages/ForgotPassword';
 import Services from './pages/Services';
 import ScheduleAppointment from './pages/ScheduleAppointment';
 import Confirmation from './pages/Confirmation';
+import Employees from './pages/Employees';
 
 function App() {
   return (
@@ -26,6 +27,7 @@ function App() {
       <Route path="/admin-account" element={<AdminAccount />} />
       <Route path="/forgot-password" element={<ForgotPassword />} />
       <Route path="/Confirmation" element={<Confirmation />} />
+      <Route path="/employees" element={<Employees />} />
     </Routes> 
   );
 }
