a50247bb13bbc4e808a56c7615ce66cb177b7566
Dillon Phan <dillonphan5@csus.edu>
2025-04-09 22:29:43 -0700

    created service status page file created, need to code and add css

---
+++ client/src/pages/Status.jsx
@@ -0,0 +1,15 @@
+import Navigation from "../components/Navigation";
+
+const Status = () => {
+    return (
+      <div className="status-page">
+      <Navigation />
+      <h1>Car Status</h1>
+  
+      <section className="status-summary">
+        </section>
+    </div>
+    );
+  };
+  
+  export default Status;
